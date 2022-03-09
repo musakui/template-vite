95632412f6a40810cac3e63a7fbac4aa99dba336
randName <kwokshungit@gmail.com>
2022-03-09 21:06:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# musakui-vite
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/musakui-vite)
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const increment = () => ++count.value
+</script>
+
+<template>
+  <div class="p-4">
+    <button class="px-2 py-0.5 rounded text-gray-300 bg-gray-600" @click="increment">
+      {{ count }} clicks
+    </button>
+  </div>
+</template>
