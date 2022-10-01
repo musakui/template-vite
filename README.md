07da961697f9ecb0751810bd236702d09a9120b9
Shun Git <kwokshungit@gmail.com>
2022-10-01 12:02:13 +0800

    add optional tsconfig

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Vite Starter Template
 
+> Starter template using [Vite](https://vitejs.dev) & [Vue](https://vuejs.org)
+
 [Open in StackBlitz](https://stackblitz.com/github/musakui/template-vite?file=src%2FApp.vue)
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup>
 import { ref } from 'vue'
+const text = ref('hello world')
 const count = ref(0)
 const increment = () => ++count.value
 </script>
 
 <template>
 	<h1 class="p-4 text-2xl">Vite Starter Template</h1>
-	<div class="p-4">
-		<div class="mb-2 text-xl">Edit me!</div>
+	<div class="p-4 text-xl text-center">Edit me!</div>
+	<div class="p-4 flex gap-2 align-center">
 		<button class="px-2 py-0.5 rounded text-gray-300 bg-gray-600" @click="increment">
 			{{ count }} clicks
 		</button>
+		<input v-model="text" class="px-1 bg-gray-700 border rounded" />
 	</div>
+	<div class="p-4 truncate">{{ text }}</div>
 </template>
