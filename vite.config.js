b304a5de45d816319f7be84c4574ab3d8eafb68e
Shun Git <kwokshungit@gmail.com>
2025-04-18 19:13:23 +0800

    use tailwind v4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import uno from 'unocss/vite'
+import wind from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		uno(),
 		vue(),
+		wind(),
 	],
 })
