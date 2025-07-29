88cd2e7ef742edac9c58763c6c41ab80d18b8a87
randName <kwokshungit@gmail.com>
2025-07-29 22:07:56 +0800

    add more components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup>
-import { ref } from 'vue'
-import { Button } from '@/components/ui/button'
-const count = ref(0)
-const increment = () => ++count.value
+import MainPage from './components/MainPage.vue'
 </script>
 
 <template>
-	<div class="p-4 flex gap-2 align-center">
-		<Button @click="increment">
-			{{ count }} clicks
-		</Button>
-	</div>
+	<MainPage />
 </template>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+const text = ref('hello world')
+const count = ref(0)
+const increment = () => ++count.value
+</script>
+
+<template>
+	<div class="h-screen flex items-center justify-center">
+		<div class="w-full max-w-sm flex flex-col gap-4">
+			<div class="grid gap-2">
+				<Label for="test">Text</Label>
+				<Input id="test" v-model="text" />
+			</div>
+			<Button @click="increment">
+				{{ count }} clicks
+			</Button>
+		</div>
+	</div>
+</template>
